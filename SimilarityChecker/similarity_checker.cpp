12a54f8b2bf181407221eb01b574efcbea769075
Beomsu Kim <beomsu7.kim@samsung.com>
2025-07-02 15:55:07 +0900

    [refactor] add length score

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -8,17 +8,24 @@ public:
 		if (isLengthGapInvalid(str1, str2)) return 0;
 		return getPartScore(str1, str2);
 	}
+
 	int getPartScore(const std::string& str1, const std::string& str2)
 	{
-		int signed_gap = str1.length() - str2.length();
-		int gap = ABS(signed_gap);
+		int gap = getGap(str1, str2);
 		int shorter_length = 
 			str1.length() < str2.length() ? str1.length() : str2.length();
 		return 60 - 60 * gap / shorter_length;
 	}
+
 	bool isLengthGapInvalid(const std::string& str1, const std::string& str2)
 	{
 		return str1.length() * 2 <= str2.length()
 			|| str1.length() >= str2.length() * 2;
+	}
+
+	int getGap(const std::string& str1, const std::string& str2)
+	{
+		int signed_gap = str1.length() - str2.length();
+		return ABS(signed_gap);
 	}
 };
